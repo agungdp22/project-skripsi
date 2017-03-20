c96bfdfe19aa6c320cb1e83bbd55c4c09f109568
agungdp22 <agungdwiprasetyo22@gmail.com>
2017-03-20 12:05:25 +0700

    get data 16x13

--- RPi.EIT-arduino/RPi.EIT-arduino.ino
+++ RPi.EIT-arduino/RPi.EIT-arduino.ino
@@ -1,3 +1,7 @@
+/*
+ *  Mikon -> Arduino MEGA
+ */
+
 #include <Wire.h>
 
 #define SLAVE_ADDRESS 0x04
@@ -10,27 +14,19 @@ int sens;
 int kirimData;
 
 // init pin multiplexer output
-int mul1[4] = {2,3,4,5};     // pin s0,s1,s2,s3 multiplexer 1
-int mul2[4] = {8,9,10,11};   // pin s0,s1,s2,s3 multiplexer 2
-int mul3[4] = {20,21,22,23}; // pin s0,s1,s2,s3 multiplexer 3
-int mul4[4] = {24,25,26,27}; // pin s0,s1,s2,s3 multiplexer 4
-int aktifMultiplex1 = 6;     // port EN multiplexer 1
-int aktifMultiplex2 = 7;     // port EN multiplexer 2
-int aktifMultiplex3 = 28;    // port EN multiplexer 3
-int aktifMultiplex4 = 29;    // port EN multiplexer 4
+int mul[4][4] = {{22,23,24,25},{26,27,28,29},{30,31,32,33},{34,35,36,37}}; // pin s0,s1,s2,s3 all multiplexer
+int ENMul[4] = {38,39,40,41};    // port EN all multiplexer
 
 float volt = 0.0;
 
 void setup() {
     Serial.begin(9600);
     for(int k=0;k<4;k++){
-        pinMode(mul1[k], OUTPUT);
-        pinMode(mul2[k], OUTPUT);
+        for(int l=0;l<4;l++){
+            pinMode(mul[k][l], OUTPUT);
+        }
+        digitalWrite(ENMul[k],LOW);
     }
-    pinMode(aktifMultiplex1, OUTPUT);
-    digitalWrite(aktifMultiplex1,LOW);
-    pinMode(aktifMultiplex2, OUTPUT);
-    digitalWrite(aktifMultiplex2,LOW);
 
     // Communication dgn Raspi
     // initialize i2c as slave
@@ -42,8 +38,18 @@ void setup() {
 }
 
 void loop(){
-    scanEIT();
-    Serial.println("\n");
+//    scanEIT();
+// masih ambil data per baris
+    for (int i = 2; i < 15; i++) { // i = kutub positif tegangan
+        delay(2000);
+        int j=i+1;                 // j = kutub negatif tegangan
+        multiplex(1, i);           // multiplexer 1 handle kutub positif tegangan
+        multiplex(2, j);           // multiplexer 2 handle kutub negatif tegangan
+        volt = getVoltage();
+        //Serial.print(j);Serial.print(" ");Serial.println(i);
+        Serial.println(volt,5);
+    }
+    Serial.println("end data\n");
     delay(4000);
 }
 
@@ -84,60 +90,64 @@ double GetTemp(void){
     return (t);
 }
 
-// EIT SECTION
+// -------------------------------------- EIT SECTION ---------------------------------------------
 
-// get data EIT (matrix 16x13)
+// get data EIT (matrix 16x13), metode tetangga
 void scanEIT(){
-    int aPos=14; // kutub positif arus injeksi
-    int aNeg=15; // kutub negatif arus injeksi
-    for(int x=0; x<16; x++){
+    // init elektroda pertama
+    int aPos = 1; // kutub positif arus injeksi
+    int aNeg = 0; // kutub negatif arus injeksi
+    int vPos = 2; // kutub positif ukur tegangan
+    int vNeg = 3; // kutub negatif ukur tegangan
+    
+    for(aNeg, aPos; aNeg<16; aNeg++, aPos++){ // 16 baris 
         if(aPos==16) aPos=0;
-        if(aNeg==16) aNeg=0;
+        Serial.print("Baris(Arus): ");Serial.print(aPos);Serial.print(" ");Serial.println(aNeg);
         
-        multiplex(3, aPos); // multiplexer 3 handle kutub positif arus injeksi
-        delay(10);
-        multiplex(4, aNeg); // multiplexer 4 handle kutub negatif arus injeksi
+        multiplex(2, aPos); // multiplexer 2 handle kutub positif arus injeksi
+        multiplex(3, aNeg); // multiplexer 3 handle kutub negatif arus injeksi
         
-        for (int i = 0; i < 13; i++) { // i = kutub positif tegangan
-            delay(1000);
-            int j=i+1;                 // j = kutub negatif tegangan
-            multiplex(1, i);           // multiplexer 1 handle kutub positif tegangan
-            delay(10);
-            multiplex(2, j);           // multiplexer 2 handle kutub negatif tegangan
+        for (int i = 0; i < 13; i++) { // get 13 data voltage (13 kolom matrix)
+            if(vPos==16) vPos=0;
+            if(vNeg==16) vNeg=0;
+//            Serial.print(vPos);Serial.print(" ");Serial.println(vNeg);
+            delay(3000);
+
+            multiplex(0, vPos); // multiplexer 0 handle kutub positif tegangan
+            multiplex(1, vNeg); // multiplexer 1 handle kutub negatif tegangan
+            
             volt = getVoltage();
-            //Serial.print(j);Serial.print(" ");Serial.println(i);
+            delay(3000);
+
             Serial.println(volt,5);
+            
+            vPos++;
+            vNeg++;
         }
-        aPos+=1;
-        aNeg+=1;
+        vPos=aPos+1;
+        vNeg=vPos+1;
+        Serial.println("\n");
     }
 }
 
 // get voltage measurement
 float getVoltage(){
-    analogReference(INTERNAL);
+    analogReference(INTERNAL1V1); // voltage reference 1.1 volt buat MEGA
     int value = analogRead(A0);
     float voltage = (value*1.1)/1023.0;
     return (voltage);
 }
 
-// mengontrol sinyal yang lewat pada multiplexer, channel decimal to biner
+// control multiplexer, change decimal to biner
 void multiplex(int tipe, int chanel){
-  int bits;
-  for(int i=0;i<4;i++){
-    bits = chanel%2;
-    chanel =(int) chanel/2;
-    
-    if(tipe==1){
-      digitalWrite(mul1[i],bits);
-    }else if(tipe==2){
-      digitalWrite(mul2[i],bits);
-    }else if(tipe==3){
-      digitalWrite(mul3[i],bits);
-    }else if(tipe==4){
-      digitalWrite(mul4[i],bits);
+    int bits;
+    for(int i=0;i<4;i++){
+        bits = chanel%2;
+        chanel =(int) chanel/2;
+
+        digitalWrite(mul[tipe][i], bits);
     }
-  }
+    delay(10);
 }
 
 /*
