c02dae3dc4948f882299a5cddd6e1a23075b97c0
agungdp22 <agungdwiprasetyo22@gmail.com>
2017-03-25 22:47:55 +0700

    send data from EIT instrument to server

--- RPi.EIT-raspberrypi/run.py
+++ RPi.EIT-raspberrypi/run.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 from config import *
-from konekArduino.I2C import I2C
+# from konekArduino.I2C import I2C
 from socketIO_client import SocketIO, LoggingNamespace
 
+import numpy as np
+from random import uniform
+
 socketIO = SocketIO(host, port, LoggingNamespace)
-arduino = I2C(address)
+# arduino = I2C(address)
+
+def getData(*args):
+	dataVolt = []
+	for i in range(208):
+		dataVolt.append(uniform(0.001,0.05))
+	print(dataVolt)
+	socketIO.emit('postDataVoltage',dataVolt)
 
 # main function
 if __name__ == '__main__':
 	socketIO.emit('raspiConnect', {'status': True})
 	print ("Listening server...")
+	socketIO.on('getDataVoltage', getData)
 	socketIO.wait()
