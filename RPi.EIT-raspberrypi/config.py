e52f41e681effaad1e35916e38504164c388cd1c
agungdp22 <agungdwiprasetyo22@gmail.com>
2017-03-25 19:00:33 +0700

    connect i2c

--- RPi.EIT-raspberrypi/config.py
+++ RPi.EIT-raspberrypi/config.py
@@ -1,2 +1,4 @@
 host = "http://localhost"
 port = 1993
+
+address = 0x68
--- RPi.EIT-raspberrypi/run.py
+++ RPi.EIT-raspberrypi/run.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from config import *
+from konekArduino.I2C import I2C
 from socketIO_client import SocketIO, LoggingNamespace
 
 socketIO = SocketIO(host, port, LoggingNamespace)
+arduino = I2C(address)
 
 # main function
 if __name__ == '__main__':
